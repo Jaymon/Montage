7249a8af63fc55047ecf92af7164b6cad264f1d5
Jay Marcyes <github@marcyes.com>
2011-10-20 02:03:52 -0700

    ReflectionDocBlock now parses docblocks

--- src/Montage/Reflection/ReflectionDocBlock.php
+++ src/Montage/Reflection/ReflectionDocBlock.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  parse a docblock comment  
+ *  parse a docblock comment 
  * 
  *  @link https://wiki.php.net/rfc/docblockparser
  *  @link http://stackoverflow.com/questions/5604587/php-annotations-addendum-or-doctrine-annotation
@@ -16,16 +16,46 @@ namespace Montage\Reflection;
 
 class ReflectionDocBlock implements \Reflector {
 
+  /**
+   *  holds the full raw docblock this instance is reflection on
+   *
+   *  @var  string   
+   */
   protected $docblock = '';
   
+  /**
+   *  during parsing, this holds the current char being parsed
+   *
+   *  @var  integer   
+   */
   protected $docblock_index = 0;
 
-  protected $docblock_index_start = 0;
+  /**
+   *  holds the bookmarked char in case the parser fails it can restart here
+   *
+   *  @var  integer   
+   */
+  protected $docblock_index_bookmark = 0;
   
+  /**
+   *  holds lines found during parsing
+   *
+   *  @var  array   
+   */
   protected $line_buffer = array();
 
+  /**
+   *  this holds the finished parsed fields from the docblock
+   *
+   *  @var  array   
+   */
   protected $field_map = array();
 
+  /**
+   *  create the object
+   *  
+   *  @param  string  $docblock the docblock to be reflected
+   */
   public function __construct($docblock){
   
     $this->parseDocBlock($docblock);
@@ -35,18 +65,29 @@ class ReflectionDocBlock implements \Reflector {
   public static function export(){ return serialize($this); }//method
   public function __toString(){ return $this->docblock; }//method
 
+  /**
+   *  bookmarks the index so parsing can try again on failure
+   */
   protected function bookmarkChar(){
   
-    $this->docblock_index_start = $this->docblock_index;
+    $this->docblock_index_bookmark = $this->docblock_index;
   
   }//method
   
+  /**
+   *  restores the previously bookmarked char
+   */
   protected function restoreChar(){
   
-    $this->docblock_index = $this->docblock_index_start;
+    $this->docblock_index = $this->docblock_index_bookmark;
   
   }//method
 
+  /**
+   *  gets the current char
+   *
+   *  @return string   
+   */
   protected function currentChar(){
   
     // canary...
@@ -58,6 +99,12 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
 
+  /**
+   *  take a look at the current index plus offset
+   *
+   *  @param  integer $offset
+   *  @return string  the char at current + offset, null if it doesn't exist   
+   */
   protected function peekChar($offset = 1){
   
     $ret_char = null;
@@ -72,6 +119,12 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
 
+  /**
+   *  move the current index to current + $offset
+   *
+   *  @param  integer $offset
+   *  @return string  the new current char   
+   */
   protected function nextChar($offset = 1){
   
     $this->docblock_index += $offset;
@@ -79,6 +132,11 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
 
+  /**
+   *  actually parse the docblock
+   *
+   *  @param  string  $docblock what's being parsed
+   */
   protected function parseDocBlock($docblock){
     
     // canary...
@@ -88,32 +146,47 @@ class ReflectionDocBlock implements \Reflector {
     $this->docblock_index = 0;
     $this->line_buffer = array();
     
-    \out::e(str_split($this->docblock));
+    ///\out::e(str_split($this->docblock));
     
     $this->parseOpen();
-    
     $this->parseShortDesc();
-    
     $this->parseLongDesc();
-    
     $this->parseTags();
-    
     $this->parseEmptyLines();
   
-    ///\out::e(substr($this->docblock,$this->docblock_index));
-    \out::e($this->field_map);
+    ///\out::b();
+    ///\out::e($this->docblock);
+    ///\out::e($this->field_map);
   
     $this->parseClose();
   
   }//method
   
+  /**
+   *  parse the close of a docblock
+   *  
+   *  close is defined as [whitespace] { * }+ /     
+   *
+   *  @return integer   
+   */
   protected function parseClose(){
   
-    $ret_count = $this->parseSpace();
+    $ret_count = $this->parseWhitespace();
     $is_valid = true;
+    $offset = 1;
     
-    if(!$this->isChar('*')){ $is_valid = false; }//if
-    if(!$this->isChar('/',1)){ $is_valid = false; }//if
+    if($this->isChar('*')){
+    
+      // allow for multi star closing docblock lines...
+      while($this->isChar('*',$offset)){ $offset++; }//while
+    
+    }else{
+    
+      $is_valid = false;
+      
+    }//if/else
+
+    if(!$this->isChar('/',$offset)){ $is_valid = false; }//if
   
     if($is_valid){
     
@@ -128,6 +201,13 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse a docblock opening
+   *  
+   *  open is defined as /** [whitespace]      
+   *
+   *  @return integer   
+   */
   protected function parseOpen(){
   
     $ret_count = 0;
@@ -155,36 +235,107 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse any @name tags from the docblock
+   *      
+   *  @return integer
+   */        
+  protected function parseTags(){
+  
+    $ret_count = 0;
+
+    try{
+
+      $this->bookmarkChar();
+  
+      while($count = $this->parseTag()){
+      
+        $ret_count += $count;
+        
+      }//if
+      
+    }catch(\DomainException $e){
+    
+      $this->restoreChar();
+    
+    }//try/catch
+     
+    return $ret_count;
+  
+  }//method
+  
+  /**
+   *  parse one @name tags from the docblock
+   *  
+   *  a tag is defined as: 
+   *    [space] , "* " , "@" , tagname , space , { string | emptyline }* , linebreak ;
+   *      
+   *  @return integer
+   */
   protected function parseTag(){
   
     $ret_count = $this->parseLineStart();
+    $name = '';
     
     if(!$this->isChar('@')){
       $this->throwParseException('Not a valid tag');
     }//if
+    
+    $this->nextChar(); // move past the @
     
     // get the name...
     do{
     
       $name .= $this->currentChar();
       $this->nextChar();
+      $ret_count++;
       
     }while(!$this->isWhitespace());
     
-    \out::e($name);
+    // finish the rest of the line...
+    $ret_count += $this->parseSpace();
+    $ret_count += $this->parseStr();
     
-    // now the rest of the line and any other lines until another tag or EOF is found
-    // will be a part of this tag
+    $this->parseEmptyLines();
+    
+    try{
+    
+      // now get any other lines that are not tag lines...
+      while(true){
+      
+        $this->bookmarkChar();
+        $ret_count += $this->parseLine();
+        $ret_count += $this->parseEmptyLines();
+        
+      }//while
+      
+    }catch(\DomainException $e){
+    
+      $this->restoreChar();
+    
+    }//try/catch
+  
+    $this->setLines($name);
+    
+    return $ret_count;
     
   }//method
   
-  protected function parseLine(){
+  /**
+   *  parse the body of a line
+   *
+   *  this isn't really a string, more like a sentence, but the grammar I was using
+   *  called it a string, so I just stuck with the same names   
+   *      
+   *  a str is defined as: { character }+
+   *  
+   *  @return integer
+   */
+  protected function parseStr(){
   
-    $ret_count = $this->parseLineStart();
+    $ret_count = 0;
     $line = '';
-    
-    if($this->isTag()){ $this->throwParseException('A Line is not a tag'); }//if
-    
+  
     while(!$this->isLinebreak()){
       
       $line .= $this->currentChar();
@@ -192,15 +343,39 @@ class ReflectionDocBlock implements \Reflector {
       $ret_count++;
     
     }//while
-    
+  
     $this->line_buffer[] = $line;
+  
+  }//method
+  
+  /**
+   *  parse a line
+   *  
+   *  defined: [space] , "*" , [space] , any character minus "@" , string , [space] , linebreak   
+   *
+   *  @return integer   
+   */        
+  protected function parseLine(){
+  
+    $ret_count = $this->parseLineStart();
+    $line = '';
     
+    if($this->isChar('@')){ $this->throwParseException('A Line is not a tag'); }//if
+    
+    $ret_count += $this->parseStr();
     $ret_count += $this->parseLinebreak();
     
     return $ret_count;
     
   }//method 
   
+  /**
+   *  parse the start of a line
+   *  
+   *  defined: [space] , "*" , [space]    
+   *
+   *  @return integer   
+   */
   protected function parseLineStart(){
   
     $ret_count = $this->parseWhitespace();
@@ -222,6 +397,11 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse all the empty lines in a row
+   *
+   *  @return integer      
+   */
   protected function parseEmptyLines(){
   
     $ret_count = 0;
@@ -232,7 +412,13 @@ class ReflectionDocBlock implements \Reflector {
       
         $this->bookmarkChar();
         $count = $this->parseEmptyLine();
-        $ret_count += $count;
+        
+        if($count > 0){
+          
+          $ret_count += $count;
+          $this->line_buffer[] = PHP_EOL;
+        
+        }//if
       
       }while($count > 0);
       
@@ -246,6 +432,13 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse an empty lines
+   *
+   *  defined: [space] , "*" , [space] , linebreak
+   *  
+   *  @return integer      
+   */
   protected function parseEmptyLine(){
   
     $ret_count = $this->parseLineStart();
@@ -255,24 +448,13 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
-  protected function isEOF(){
-    
-    $ret_bool = false;
-    
-    if($this->isChar('*')){
-    
-      if($this->isChar('/')){
-      
-        $ret_bool = true;
-      
-      }//if
-    
-    }//if
-    
-    return $ret_bool;
-  
-  }//method
-  
+  /**
+   *  parse a line break
+   *
+   *  defined: "\n" | "\r" | "\r\n"
+   *  
+   *  @return integer      
+   */
   protected function parseLinebreak(){
   
     $ret_count = 0;
@@ -308,6 +490,11 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  true if the next char(s) is a line break
+   *  
+   *  @return boolean
+   */
   protected function isLinebreak(){
   
     $ret_bool = false;
@@ -331,10 +518,16 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse a long description
+   *  
+   *  defined: line , { line | emptyline }*
+   *  
+   *  #return integer      
+   */
   protected function parseLongDesc(){
   
     $ret_count = 0;
-    ///$this->line_buffer = array();
   
     try{
     
@@ -349,7 +542,6 @@ class ReflectionDocBlock implements \Reflector {
     }catch(\DomainException $e){
     
       $this->restoreChar();
-      \out::e($e->getMessage());
     
     }//try/catch
   
@@ -359,7 +551,18 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse the short description
+   *  
+   *  the short desc is usually the first line of a multiline docblock
+   *  
+   *  defined: line , { emptyline }+            
+   *
+   *  @return integer   
+   */
   protected function parseShortDesc(){
+  
+    $ret_count = 0;
   
     try{
     
@@ -384,6 +587,13 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse spaces
+   *  
+   *  defined:  " " | "\t"      
+   *
+   *  return  integer   
+   */
   protected function parseSpace(){
   
     $ret_count = 0;
@@ -400,6 +610,13 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  parse whitespace
+   *  
+   *  whitespace is any space including linebreaks      
+   *
+   *  @return integer   
+   */
   protected function parseWhitespace(){
   
     $ret_count = 0;
@@ -415,10 +632,20 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
 
-  protected function isTag(){ return $this->isChar('@'); }//method
-
+  /**
+   *  true if the current char is a whitespace character
+   *  
+   *  @return boolean
+   */
   protected function isWhitespace(){ return ctype_space($this->currentChar()); }//method
 
+  /**
+   *  true if the current char matches $expected char
+   *  
+   *  @param  string  $expected_char
+   *  @param  integer $offset if you want to see another char besides the current char
+   *  @return boolean
+   */
   protected function isChar($expected_char,$offset = 0){
   
     $ret_bool = true;
@@ -434,6 +661,14 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  throw a parse exception
+   *  
+   *  this just takes the $msg and adds more info and throws a DomainException
+   *  
+   *  @throws \DomainException            
+   *  @param  string  $msg
+   */
   protected function throwParseException($msg){
   
     $msg .= '. Remaining DocBlock: '.mb_substr($this->docblock,$this->docblock_index);
@@ -442,9 +677,29 @@ class ReflectionDocBlock implements \Reflector {
   
   }//method
   
+  /**
+   *  set any lines in the $line_buffer into the $key
+   *  
+   *  @param  string  $key
+   */
   protected function setLines($key){
   
-    $this->field_map[$key] = join(PHP_EOL,$this->line_buffer);
+    // canary...
+    if(empty($this->line_buffer)){ return; }//if
+  
+    $str = trim(join(PHP_EOL,$this->line_buffer));
+    
+    if(isset($this->field_map[$key])){
+    
+      $this->field_map[$key] = (array)$this->field_map[$key];
+      $this->field_map[$key][] = $str;
+    
+    }else{
+    
+      $this->field_map[$key] = $str;
+    
+    }//if/else
+  
     $this->line_buffer = array();
     
   }//method
--- test/Montage/PHPUnit/Reflection/ReflectionDocBlockTest.php
+++ test/Montage/PHPUnit/Reflection/ReflectionDocBlockTest.php
@@ -24,6 +24,79 @@ class ReflectionFileTest extends FrameworkTestCase {
   
     $test_list = array();
     $test_list[] = array(
+      'in' => '/'.'**
+      * this is the variable description
+      * 
+      * @var  \Path\To\The\Class                              
+      ********************************************************************'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'**
+      * this is the variable description
+      * 
+      * @var  \Path\To\The\Class                              
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'**
+      * 
+      * @param  boolean $one  this is a mutli-line description
+      *                       that has some more text
+      *                       
+      * and a little more text                  
+      * 
+      * @param  string $two  description                  
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'**
+      * 
+      * @param  boolean $one  this is a mutli-line description
+      *                       that has some more text      
+      * 
+      * @param  string $two  description                  
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'**
+      * this is the short description
+      * 
+      * this is the long description.
+      * and this also.
+      * 
+      * and also this.
+      *                      
+      * @param  boolean $one  this is a mutli-line description
+      *                       that has some more text      
+      * @param  string $two  description                  
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'**
+      * this is the short description
+      * 
+      * this is the long description.
+      * and this also.
+      * 
+      * and also this.
+      *                      
+      * @param  boolean $one  description
+      * @param  string $two  description                  
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
+      'in' => '/'.'** 
+      * @param  boolean $is_there true if there, false otherwise            
+      *'.'/',
+      'out' => array()
+    );
+    $test_list[] = array(
       'in' => '/'.'** 
       * this will be the multi-line long description
       * and here is the second line
@@ -54,7 +127,8 @@ class ReflectionFileTest extends FrameworkTestCase {
     foreach($test_list as $i => $test_map){
     
       $rdc = new ReflectionDocBlock($test_map['in']);
-      return;
+      
+      \out::i($rdc);
       
       ///$this->assertEquals($test_map['out'],$rfile->getClasses(),$i);
     
