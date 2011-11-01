3af98af2137586270cd28893a8f496fe62f6d731
Jay Marcyes <github@marcyes.com>
2011-10-31 18:31:56 -0700

    adds getParsedTag() to ReflectionDocBlock class. Currently, it only supports var, param, and return tags but I'm sure it will be fleshed out more as time goes on and I need more functionality

--- test/Montage/PHPUnit/Reflection/ReflectionDocBlockTest.php
+++ test/Montage/PHPUnit/Reflection/ReflectionDocBlockTest.php
@@ -121,5 +121,96 @@ class ReflectionFileTest extends FrameworkTestCase {
     }//foreach
   
   }//method
+  
+  /**
+   *  test parsing certain supported tags
+   *  
+   *  tags are parsed according to this list: http://en.wikipedia.org/wiki/PHPDoc#Tags      
+   *
+   *  @since  10-31-11   
+   */
+  public function testGetParsedTag(){
+  
+    $test_list = array();
+    $test_list['one full param'] = array(
+      'in' => '/'.'**
+        * this is the variable description
+        * 
+        * @param  string  $varname  desc
+        *'.'/',
+      'tag' => 'param',
+      'out' => array(array(
+        'type' => 'string',
+        'varname' => '$varname',
+        'desc' => 'desc'
+      ))
+    );
+    $test_list['one param no name'] = array(
+      'in' => '/'.'**
+        * this is the variable description
+        * 
+        * @param  string  desc
+        *'.'/',
+      'tag' => 'param',
+      'out' => array(array(
+        'type' => 'string',
+        'varname' => '',
+        'desc' => 'desc'
+      ))
+    );
+    $test_list['two full params'] = array(
+      'in' => '/'.'**
+        * this is the variable description
+        * 
+        * @param  string  $varname  one desc
+        * @param  array $var2 this is another desc        
+        *'.'/',
+      'tag' => 'param',
+      'out' => array(
+        array(
+          'type' => 'string',
+          'varname' => '$varname',
+          'desc' => 'one desc'
+        ),
+        array(
+          'type' => 'array',
+          'varname' => '$var2',
+          'desc' => 'this is another desc'
+        )
+      )
+    );
+    $test_list['Simple var no description'] = array(
+      'in' => '/'.'**
+        * this is the variable description
+        * 
+        * @var  ClassName
+        *'.'/',
+      'tag' => 'var',        
+      'out' => array(
+        'type' => 'ClassName',
+        'desc' => ''
+      )
+    );
+    $test_list['Simple var with description'] = array(
+      'in' => '/'.'**
+        * this is the variable description
+        * 
+        * @var  ClassName this is the classname description
+        *'.'/',
+      'tag' => 'var',        
+      'out' => array(
+        'type' => 'ClassName',
+        'desc' => 'this is the classname description'
+      )
+    );
+    
+    foreach($test_list as $msg => $test_map){
+    
+      $rdc = new ReflectionDocBlock($test_map['in']);
+      $this->assertEquals($test_map['out'],$rdc->getParsedTag($test_map['tag']),$msg);
+    
+    }//foreach
+  
+  }//method
 
 }//class
